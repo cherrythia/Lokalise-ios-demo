0c5fdaae328094d1fb0ca0004fa540c039ba17a7
Terry Chia <chiawz@hotmail.com>
2023-02-20 09:46:35 +0800

    Initial Commit

---
+++ Lokalise-iOS-Demo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lokalise-iOS-Demo
+//
+//  Created by Chia Wei Zheng Terry on 20/2/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Lokalise-iOS-DemoUITests/Lokalise_iOS_DemoUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Lokalise_iOS_DemoUITestsLaunchTests.swift
+//  Lokalise-iOS-DemoUITests
+//
+//  Created by Chia Wei Zheng Terry on 20/2/23.
+//
+
+import XCTest
+
+final class Lokalise_iOS_DemoUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
